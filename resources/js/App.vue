4290564ff8780cbc598c6a8d8ee9a1610c16c428
Phuc Doan <phuc.doan@urani.solutions>
2025-10-30 16:27:20 +0700

    init

---
+++ resources/js/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div class="max-w-md w-full space-y-8">
+      <div>
+        <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          {{ msg }}
+        </h2>
+        <p class="mt-2 text-center text-sm text-gray-600">
+          Current count is {{ count }}
+        </p>
+      </div>
+      <div>
+        <button
+          @click="count++"
+          class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        >
+          Click me
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Hello Vue in Laravel!',
+      count: 0
+    }
+  }
+}
+</script>
